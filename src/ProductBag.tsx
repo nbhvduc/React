bb81f23287ce0b287c9b8aaf9164aa794621ec6b
nbhvduc <nguyenbaohoaivietduc10@gmail.com>
2026-07-30 04:42:19 +0900

    baitap2

--- src/ProductBag.tsx
+++ src/ProductBag.tsx
@@ -6,15 +6,19 @@ export function ProductBag({image,producttitle,price}:any){
     return(
 
         <div className="product_bag_container">
-            <img className='product_image' src={image} alt="Bag"/>
-            <div className="title_product">
-                <h4>{producttitle}</h4>
-                <span className="product_price">{price}</span>
+            <div className='edit_content_product'>
+                <img className='product_image' src={image} alt="Bag"/>
+                <div className="title_product">
+                    <h4>{producttitle}</h4>
+                    <span className="product_price">{price}</span>
+                </div>
             </div>
-          
-           <button onClick={()=>setCount(count+1)}>Add to bag({count})</button>
+            <div className='edit_btn'>
+                <button className='first_button' onClick={()=>setCount(count+1)}>Add to bag({count})</button>
+                <button className='second_button' onClick={()=>{if(count>0) setCount(count-1)}}>Undo</button>
+            </div>
         </div>
-    )
+    );
 
     
 }
