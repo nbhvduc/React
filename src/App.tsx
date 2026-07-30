5fbc6fabe19c9fe6e9997fd57be2ff2dd6021453
huy.duong <huy.duong@x-hr.co>
2026-07-30 21:32:33 +0700

    css basic

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,13 @@ import './App.css'
 import Bag1 from './assets/images/image1.png'
 import Bag2 from './assets/images/image2.png'
 import { ProductBag } from './ProductBag'
+import { Cssbasic } from './Cssbasic'
 
 function App() {
   return (
     <div>
-     <ProductBag image={Bag1} 
-     producttitle='キャンバス ショルダーバッグ 韓国 大容量 A4 通学 通勤 レディース メンズ'
-     price="￥5000"/>
-
-     <ProductBag image={Bag2}
-     producttitle="ショルダーバッグ キャンバス地 クリアポケット付き クマちゃん付き 斜めがけ"
-     price='￥4000'/>
-
-     
+  
+    <Cssbasic/>
     </div>
   
    
