7c1e8a593a8fe94efb944f1bab6e5579c55e2274
nbhvduc <nguyenbaohoaivietduc10@gmail.com>
2026-08-24 00:34:56 +0900

    create css menu

--- src/Pages/Menu/menu.tsx
+++ src/Pages/Menu/menu.tsx
@@ -1,11 +1,28 @@
-import "./menu.css";
+import { useState } from "react";
 import menuSticker from "../../assets/menu-sticker.png";
+import "./menu.css";
 
 export function Menu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function hanldeToggleMenu() {
+    setIsMenuOpen((prev) => !prev);
+  }
+
+  const classMenu = ["menu", isMenuOpen ? "menu-open" : ""].join(" ");
+  console.log(classMenu);
+
   return (
-    <div>
+    <div className="container">
+      <div className={classMenu}>
+        <ul>
+          <li onClick={hanldeToggleMenu}>WEB給与明細</li>
+          <li onClick={hanldeToggleMenu}>パスワード・メールアドレス設定</li>
+        </ul>
+      </div>
+
       <div className="header">
-        <img src={menuSticker} alt="menu-Sticker" />
+        <img onClick={hanldeToggleMenu} src={menuSticker} alt="menu-Sticker" />
         <h3 className="title-menu">勤怠管理アプリ</h3>
       </div>
     </div>
